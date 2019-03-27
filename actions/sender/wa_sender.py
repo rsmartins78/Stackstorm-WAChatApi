2963314bee1ef6b24423eaf97c744ed0a5becfba
Rafael Martins <rsmartins78@hotmail.com>
2019-03-27 15:35:36 -0300

    corrections

--- actions/sender/wa_sender.py
+++ actions/sender/wa_sender.py
@@ -2,11 +2,11 @@ import requests
 
 class whatsappchatapi:
 
-    def send_message(instance,token,chatId,message):
+    def send_message(instance,token,chat_id,message):
 
         url = "https://eu21.chat-api.com/"
 
-        data = {'chatId':chatId, 'body':message}
+        data = {'chatId':chat_id, 'body':message}
 
         r = requests.post(url=url+instance+'/sendMessage?token='+token, json=data.encode('utf8'))
 

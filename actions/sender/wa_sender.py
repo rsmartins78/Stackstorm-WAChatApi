a11a1f68c5ae343b87794985a840b454b7782364
Rafael Martins <rsmartins78@hotmail.com>
2019-03-27 16:05:10 -0300

    encoding failure on stackstorm corrected

--- actions/sender/wa_sender.py
+++ actions/sender/wa_sender.py
@@ -5,9 +5,9 @@ def wasend_message(instance,token,chat_id,message):
 
     url = "https://eu21.chat-api.com/"
 
-    data = {'chatId':str(chat_id), 'body':str(message)}
+    data = {'chatId':chat_id, 'body':message}
 
-    r = requests.post(url=url+str(instance)+'/sendMessage?token='+str(token), json=data.encode('utf8'))
+    r = requests.post(url=url+str(instance)+'/sendMessage?token='+str(token), json=data)
 
     response_text = r.text
 

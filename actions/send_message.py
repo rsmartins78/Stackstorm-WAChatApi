024d5b21f794f876b23f2c1ca11d19a0f311abe8
Rafael Martins <rsmartins78@hotmail.com>
2019-03-27 14:30:13 -0300

    removing comments

--- actions/send_message.py
+++ actions/send_message.py
@@ -1,12 +1,7 @@
 from sender.wa_sender import WASendMessage
 from emoji import emojize
-
 from st2common.runners.base_action import Action
 
-# instance = "instance32573"
-# token = "pkdahmmvixtcmajp"
-# chatId = '5511952218967@c.us'
-# message = 'É emoji que vc quer?? :bear:'
 
 class WhatsappSendMessageAction(Action):
     def run(self, chatId, message):

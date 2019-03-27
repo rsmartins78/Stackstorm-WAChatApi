a41ce1338ae3acc878a729adc5ab9112ed13dab1
Rafael Martins <rsmartins78@hotmail.com>
2019-03-27 15:56:59 -0300

    correction on function name

--- actions/send_message.py
+++ actions/send_message.py
@@ -5,6 +5,6 @@ from st2common.runners.base_action import Action
 
 class WhatsappSendMessageAction(Action):
     def run(self, chat_id, message):
-        message = send_message(instance=self.config['api_instance'],token=self.config['api_token'],chat_id=chat_id,message=emojize(message,use_aliases=True))
+        message = wasend_message(instance=self.config['api_instance'],token=self.config['api_token'],chat_id=chat_id,message=emojize(message,use_aliases=True))
         return message
 
